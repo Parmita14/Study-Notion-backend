6ee1a2142bf85fa747728d4510fe75370f94c306
Parmita14 <parmitaupadhyay3@gmail.com>
2024-01-27 23:56:40 +0530

    first-commit

---
+++ utils/mailSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const mailSender = async (email, title, body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: `<parmitaupadhyay3@gamil.com>`,
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+
+
+module.exports = mailSender;
